699c66f51f4d1cdc3613eb3c663d30e2a2d0ec61
Will Quinnan <wmplqnjr@gmail.com>
2023-03-16 17:03:38 -0700

    Front end

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,8 @@
-from flask import Flask, render_template
+from flask import (
+    Flask, 
+    render_template,
+    request
+)
 from datetime import datetime
 import requests
 
@@ -17,5 +21,34 @@ def show_tasks():
     resp = requests.get(url)
     if resp.status_code == 200:
         task_list = resp.json().get("tasks")
-        render_template("task_list.html"), tasks=task_list
+        return render_template("task_list.html", tasks=task_list)
+    return render_template("error.html"), resp.status_code
+
+@app.get("/tasks/<int:pk>")
+def task_detail(pk):
+        url = "%s/tasks/%s" % (BACKEND_URL, pk)
+        resp = requests.get(url)
+        if resp.status_code ==200:
+            task = resp.json().get(url)
+            return render_template("task_detail.html", task=task)
+        return render_template("error.html"), resp.status_code
+
+
+@app.get("/tasks/new")
+def new_task_form():
+    return render_template("new.html")
+
+
+@app.post("/tasks/new")
+def create_task():
+    task_data = {
+        "summary": request.form.get("summary"),
+        "description": request.form.get("description"),
+        "status_id": request.form.get("status_id"),
+        "active": request.form.get("active")
+    }
+    url = "%s/tasks" % BACKEND_URL
+    resp = requests.post(url, json=task_data)
+    if resp.status_code == 204:
+        return render_template("success.html")
     return render_template("error.html"), resp.status_code
